a969b820060c26b3bff2dd106020ea41b65382ab
Aleks Rutins <aleks@rutins.com>
2021-11-10 11:15:37 -0500

    Add math.angleTo, add some docs

--- math.js
+++ math.js
@@ -1,3 +1,20 @@
+/**
+ * Calculates a linear interpolation.
+ * @param {number} x 
+ * @param {number} y 
+ * @param {number} t 
+ * @returns 
+ */
 export function lerp(x, y, t) {
 	return x*t + y*(1-t);
 }
+
+/**
+ * Calculates the angle from one point to another.
+ * @param {{x: number, y: number}} origin 
+ * @param {{x: number, y: number}} target 
+ * @returns {number}
+ */
+export function angleTo(origin, target) {
+    return Math.atan2(target.y - origin.y, target.x - origin.x);
+} 
--- sprite.js
+++ sprite.js
@@ -1,9 +1,22 @@
+/**
+ * 
+ * @param {IWorldInstance} sprite 
+ * @returns {boolean}
+ */
 export function isOutsideLayout(sprite) {
     return (sprite.x < 0 || sprite.y < 0 || sprite.x > sprite.layout.width || sprite.y > sprite.layout.height);
 }
 export function destroyAll(...sprites) {
     return Promise.allSettled(sprites.map(sprite => sprite.destroy()));
 }
+/**
+ * @typedef IObjectClass
+ * @typedef IWorldInstance
+ * @param {number | string} layer 
+ * @param {{x: number, y: number}} respawnPoint 
+ * @param {...IObjectClass} sprites
+ * @returns {Generator<IWorldInstance>}
+ */
 export function* respawnAll(layer, respawnPoint, ...sprites) {
     for (let sprite of sprites) {
         yield sprite.createInstance(layer, ...Object.values(respawnPoint), false);
