d40bbe88b0c633751beb3258271777069931087c
Aleks Rutins <aleks@rutins.com>
2021-11-11 10:22:29 -0500

    [ui] use a getter for Renderable#content

--- ui.js
+++ ui.js
@@ -24,7 +24,9 @@ export function uiText(item) {
     }
 }
 export class Renderable {
-    content = '';
+    get content() {
+        return '';
+    }
     /**
      * @type {{color: string, backColor: string}}
      */
