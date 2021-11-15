490d78914baf60e56d59da4e39b43046a86f47ef
Aleks Rutins <aleks@rutins.com>
2021-11-15 10:36:01 -0500

    Add audio utilities

---
+++ audio.js
@@ -0,0 +1,10 @@
+let preloads = new Map();
+
+async function preload(fileName) {
+	preloads.set(fileName, await runtime.assets.getMediaFileUrl(fileName));
+}
+async function playAudio(fileName) {
+	const url = (preloads.has(fileName)? preloads.get(fileName) : await runtime.assets.getMediaFileUrl(fileName));
+	let el = new Audio(url);
+	el.play();
+}
