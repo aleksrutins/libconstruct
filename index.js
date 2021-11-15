e925eff8f802f455bfc9af3788cd08f9dc92d2bb
Aleks Rutins <aleks@rutins.com>
2021-11-15 10:37:39 -0500

    Export audio utils

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * as ui from './ui.js';
 export * as math from './math.js';
 export * as physics from './physics.js';
 export * as sprite from './sprite.js';
+export * as audio from './audio.js';
