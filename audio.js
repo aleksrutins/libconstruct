277b85339d84f1294608af50bd8fee88f93467a6
Aleks Rutins <aleks@rutins.com>
2021-11-15 10:47:06 -0500

    Shorten method name

--- audio.js
+++ audio.js
@@ -3,7 +3,7 @@ let preloads = new Map();
 async function preload(fileName) {
 	preloads.set(fileName, await runtime.assets.getMediaFileUrl(fileName));
 }
-async function playAudio(fileName) {
+async function play(fileName) {
 	const url = (preloads.has(fileName)? preloads.get(fileName) : await runtime.assets.getMediaFileUrl(fileName));
 	let el = new Audio(url);
 	el.play();
