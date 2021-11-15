ab93d7a17d9581fc75022acd1843fedacacdc772
Aleks Rutins <aleks@rutins.com>
2021-11-15 10:53:25 -0500

    Remember to export

--- audio.js
+++ audio.js
@@ -1,9 +1,9 @@
 let preloads = new Map();
 
-async function preload(fileName) {
+export async function preload(fileName) {
 	preloads.set(fileName, await runtime.assets.getMediaFileUrl(fileName));
 }
-async function play(fileName) {
+export async function play(fileName) {
 	const url = (preloads.has(fileName)? preloads.get(fileName) : await runtime.assets.getMediaFileUrl(fileName));
 	let el = new Audio(url);
 	el.play();
