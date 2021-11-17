2f3f85e48e4e1dc5c24d1ae1f88b1af6a8e56457
Aleks Rutins <aleks@rutins.com>
2021-11-17 10:27:53 -0500

    No short-circuit conditionals

--- audio.js
+++ audio.js
@@ -3,8 +3,17 @@ let preloads = new Map();
 export async function preload(runtime, fileName) {
 	preloads.set(fileName, await runtime.assets.getMediaFileUrl(fileName));
 }
-export async function play(runtime, fileName) {
-	const url = (preloads.has(fileName)? preloads.get(fileName) : await runtime.assets.getMediaFileUrl(fileName));
+export async function play(fileName, runtime) {
+	let url;
+	if(preloads.has(fileName)) {
+		url = preloads.get(fileName);
+	}
+	else if(runtime != null) {
+		url = await runtime.assets.getMediaFileUrl(fileName);
+	} else {
+		throw new Error("Audio is not preloaded and cannot load from runtime");
+		return;
+	}
 	let el = new Audio(url);
 	el.play();
 }
